e0f815491450fa23bd1942ff12ef22308f700562
starry <76326452@qq.com>
2016-05-24 11:33:35 +0800

    add

--- OS/mutilProcess.py
+++ OS/mutilProcess.py
@@ -7,6 +7,11 @@ def run_proc(name):
     print('run child process %s (%s)'(name, os.getpid()))
 
 
+'''
+创建子进程时，只需要传入一个执行函数和函数的参数，创建一个Process实例，用start()方法启动，这样创建进程比fork()还要简单。
+
+join()方法可以等待子进程结束后再继续往下运行，通常用于进程间的同步
+'''
 print('Parent process %s.' % os.getpid())
 p = Process(target=run_proc, args=('test',))
 print('Child process will start.')
