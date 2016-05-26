459a202d6c2d5d1e8964913f71c365ac58de54bd
starry <76326452@qq.com>
2016-05-26 15:51:18 +0800

    win7 分布式进程 模拟

---
+++ OS/multiThread.py
@@ -0,0 +1,35 @@
+import time, threading
+
+'''
+Python的标准库提供了两个模块：_thread和threading，_thread是低级模块，threading是高级模块，对_thread进行了封装。绝大多数情况下，我们只需要使用threading这个高级模块。
+'''
+
+
+def loop():
+    print('thread %s is running...' % threading.current_thread().name)
+    n = 0;
+    while n < 5:
+        n = n + 1
+        print('thread %s >>> %s' % (threading.current_thread().name, n))
+        time.sleep(1)
+    print('thread %s end' % threading.current_thread().name)
+
+
+print('thread %s is running...' % threading.current_thread().name)
+t = threading.Thread(target=loop, name='LoopThread')
+t.start()
+t.join()
+print('thread %s ended.' % threading.current_thread().name)
+
+'''
+#output:
+thread MainThread is running...
+thread LoopThread is running...
+thread LoopThread >>> 1
+thread LoopThread >>> 2
+thread LoopThread >>> 3
+thread LoopThread >>> 4
+thread LoopThread >>> 5
+thread LoopThread end
+thread MainThread ended.
+'''
--- OS/mutilProcess.py
+++ OS/mutilProcess.py
@@ -12,9 +12,10 @@ def run_proc(name):
 
 join()方法可以等待子进程结束后再继续往下运行，通常用于进程间的同步
 '''
-print('Parent process %s.' % os.getpid())
-p = Process(target=run_proc, args=('test',))
-print('Child process will start.')
-p.start()
-p.join()
-print('Child process end.')
+if __name__ == '__main__':
+    print('Parent process %s.' % os.getpid())
+    p = Process(target=run_proc, args=('test',))
+    print('Child process will start.')
+    p.start()
+    p.join()
+    print('Child process end.')
