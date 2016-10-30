e3943a13a2f21e3a6074c11939952e4a0adc471d
starry001 <wang790494292>
2016-10-30 17:58:39 +0800

    oop

--- ErrorAndException/__init__.py
+++ ErrorAndException/__init__.py
@@ -34,3 +34,4 @@ s = '0'
 n = int(s)
 logging.info('n = %d' % n)
 print(10 / n)
+
---
+++ inbuild_module/generateDemo.py
@@ -0,0 +1,31 @@
+'八皇后问题'
+
+import random
+
+def conflict(state, nextX):
+    nextY = len(state)
+    for i in range(nextY):
+        if abs(state[i] - nextX) in (0, nextY - i):
+            return True
+    return False
+
+
+def queen(num=8, state=()):
+    for pos in range(num):
+        if not conflict(state, pos):
+            if len(state) == num - 1:
+                yield (pos,)
+            else:
+                for result in queen(num, state + (pos,)):
+                    yield (pos,) + result
+
+
+def prettyprint(solution):
+    def line(pos, length=len(solution)):
+        return '.' * (pos) + 'X' + '.' * (length - pos - 1)
+
+    for pos in solution:
+        print(line(pos))
+
+
+prettyprint(random.choice(list(queen(8))))
